457b7bb4933353825ba80fcfaebeffcc58a97373
Ali Bagheri <khodealib@gmail.com>
2023-10-24 13:24:20 +0330

    django messages

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.http.request import HttpRequest
-
 from django.shortcuts import render, redirect
 
 from home.models import Todo
@@ -17,5 +17,12 @@ def todo_detail(request: HttpRequest, todo_id: int) -> HttpResponse:
 
 
 def todo_delete(request: HttpRequest, todo_id: int) -> HttpResponse:
+    """
+    send extra tags for css style in django use extra on messages
+    messages.success(request, "TodoModel deleted successfully!", "success")
+    and use in django templated with message.tags
+    this here we use bootstrap messages!
+    """
     Todo.objects.get(id=todo_id).delete()
+    messages.success(request, "Todo deleted successfully!")
     return redirect("home:home")
